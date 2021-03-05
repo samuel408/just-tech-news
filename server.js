f18f64c485e04d534a4c78fbf14346410527d0a6
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-03-04 20:46:35 -0800

    added api route for posts

--- server.js
+++ server.js
@@ -14,4 +14,20 @@ app.use(routes);
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
+});const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
