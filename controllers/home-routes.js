18a5ad3af516e623f5cbfb87f988648c39a38c77
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-03-09 20:15:39 -0800

    added back end session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 router.get('/', (req, res) => {
+
+  console.log(req.session);
+
   Post.findAll({
     attributes: [
       'id',
@@ -30,12 +33,22 @@ router.get('/', (req, res) => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
       
       res.render('homepage', { posts });
+   
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+
 });;
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');});
   
 
 module.exports = router;
